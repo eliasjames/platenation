08b3bdde7414c64c2d8d352b94d64d0ea3676faf
Elias Carlston <elias@eliascarlston.com>
2013-02-19 16:41:02 -0600

    Blank Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platenation::Application.config.secret_token = '98c4e8245acb29b7cd4eb810fcca51e8eea3279b8d9eec78079759fd98730a7448b420fec348523989230bc753a690d4c4474c12cba965f9cca2a157c0e09768'
